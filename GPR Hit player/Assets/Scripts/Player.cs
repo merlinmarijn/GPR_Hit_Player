14d33b47a4a6eb7b4b7de41a6dd9a265bd1f9162
merlinmarijn <ratched10@hotmail.nl>
2020-02-28 09:59:41 +0100

    First Upload

---
+++ GPR Hit player/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private int health = 100;
+    private Material mat;
+    private Color CD;
+    public Text healthText;
+    private Animator anim;
+
+
+    private void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        CD = mat.color;
+        healthText.text = health.ToString();
+        anim = GetComponent<Animator>();
+    }
+
+    public void DetectHit(int damage)
+    {
+        health -= damage;
+        anim.SetTrigger("Hit");
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        StartCoroutine(HitEffect());
+    }
+
+    public IEnumerator HitEffect()
+    {
+        healthText.text = health.ToString();
+        yield return null;
+    }
+}
---
+++ GPR Hit player/Assets/Scripts/Raycast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    Raycast ray;
+    public GameObject prefab;
+    void Update()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(transform.forward)*100, Color.red);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == "Player")
+                {
+                    hit.transform.GetComponent<Player>().DetectHit(10);
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(prefab);
+        }
+    }
+}
